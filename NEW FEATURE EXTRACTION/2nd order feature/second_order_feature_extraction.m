459603152b65110da9d91511f4342a630a8efcc9
flarefansy <31630361+flarefansy@users.noreply.github.com>
2018-04-02 00:15:26 -0500

    Add files via upload

--- NEW FEATURE EXTRACTION/2nd order feature/second_order_feature_extraction.m
+++ NEW FEATURE EXTRACTION/2nd order feature/second_order_feature_extraction.m
@@ -1,3 +1,29 @@
 clear;
 
-system('./TAppDecoderStatic -b second_order features/Chillin_Like_a_Villain_(From_Descendants_2)_000_0_125_1920x1080_HW_Q3_CRF10.bin');
+clear;
+
+file = dir('mv/*.bin');
+[num_sequence a] = size(file);
+resolution = '854x480';
+num_s = 0;
+num_w = 0;
+
+for j = 1:num_sequence
+    a = strfind(file(j).name,resolution);
+    if isempty(a) == 0
+        num_s = num_s+1;
+        
+        copyfile(sprintf('mv/%s',file(j).name),'test/1.bin');
+        
+        system('./TAppDecoderStatic -b test/1.bin >1.txt');
+        sequence_name(num_s) = string(file(j).name);
+    else
+        num_w = num_w+1;
+    end
+    
+    delete('test/*.bin');
+end
+
+datacolumns = {'Name'};
+data = table(sequence_name);
+writetable(data,'second_order_features_name');
